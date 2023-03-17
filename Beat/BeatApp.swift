e39318aaee38736c8e07bb19255d9ac4e0ade2d0
AtlanticNova <idiotcolony97@gmail.com>
2023-03-17 18:04:37 +0700

    Initial Commit

---
+++ Beat/BeatApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeatApp.swift
+//  Beat
+//
+//  Created by Martinus Andika Novanawa on 17/03/23.
+//
+
+import SwiftUI
+
+@main
+struct BeatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
